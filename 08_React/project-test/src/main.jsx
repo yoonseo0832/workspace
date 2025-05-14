ed06dfc6a063f20e483f5715feaf7b04f0004d5d
yoonseo <145835135+yoonseo0832@users.noreply.github.com>
2025-05-14 17:50:30 +0900

    0514

--- 08_React/project-test/src/main.jsx
+++ 08_React/project-test/src/main.jsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React, { useEffect, useState } from "react";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const { kakao } = window;
+
+const Main = () => {
+  const [map, setMap] = useState(null);
+
+  //처음 지도 그리기
+  useEffect(() => {
+    const container = document.getElementById("map");
+    const options = { center: new kakao.maps.LatLng(33.450701, 126.570667) };
+    const kakaoMap = new kakao.maps.Map(container, options);
+    setMap(kakaoMap);
+  }, []);
+
+  return (
+    <div
+      style={{
+        width: "100%",
+        display: "inline-block",
+        marginLeft: "5px",
+        marginRight: "5px",
+      }}
+    >
+      <div id="map" style={{ width: "99%", height: "500px" }}></div>
+    </div>
+  );
+};
+export default Main;
