515dfe86fd03da8f7541afdfc45058486098a360
yoonseo <145835135+yoonseo0832@users.noreply.github.com>
2025-05-14 10:58:45 +0900

    00

---
+++ 07_framework/02_spring/mapAndStay/src/main/java/com/kh/client/MapAndStayApplication.java
@@ -0,0 +1,13 @@
+package com.kh.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapAndStayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapAndStayApplication.class, args);
+	}
+
+}
